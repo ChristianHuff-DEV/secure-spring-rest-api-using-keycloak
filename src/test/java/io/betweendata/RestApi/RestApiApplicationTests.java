22e250364d8912cd51b4ec9fbb8ab5270abd24bc
Christian Huff <christian.huff.dev@gmail.com>
2023-02-17 21:54:49 +0000

    Setup Spring Boot project

---
+++ src/test/java/io/betweendata/RestApi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.betweendata.RestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
