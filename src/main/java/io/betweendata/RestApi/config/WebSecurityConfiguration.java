44d85b0c6c503fb4d3b58111b2df1cc466d869df
Christian Huff <christian.huff.dev@gmail.com>
2023-02-19 14:37:58 +0000

    Require an access token with scope "email" to access the endpoints

--- src/main/java/io/betweendata/RestApi/config/WebSecurityConfiguration.java
+++ src/main/java/io/betweendata/RestApi/config/WebSecurityConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.DelegatingJwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +15,18 @@ public class WebSecurityConfiguration {
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.authorizeHttpRequests().requestMatchers("/**").permitAll();
+
+    DelegatingJwtGrantedAuthoritiesConverter authoritiesConverter =
+            new DelegatingJwtGrantedAuthoritiesConverter(new JwtGrantedAuthoritiesConverter());
+
+    httpSecurity.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwt -> new JwtAuthenticationToken(jwt,
+            authoritiesConverter.convert(jwt)));
+
+    httpSecurity.authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/**")
+            .hasAuthority("SCOPE_email")
+            .anyRequest()
+            .authenticated());
 
     return httpSecurity.build();
   }
